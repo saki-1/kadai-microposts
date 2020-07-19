bf3af1eb8f5188a11e670d62a1192f2fdfe257f0
saki.inoue <s3-inoue@nri-net.com>
2020-07-19 14:49:12 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,9 +39,16 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.fav_microposts.page(params[:page])
+    counts(@user)
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
